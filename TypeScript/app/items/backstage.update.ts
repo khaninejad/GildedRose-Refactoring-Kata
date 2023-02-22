31ae1c9eb89722876fe85ee4596f5b150adea816
Payam <payam.khaninejad@coinhaven.com>
2023-02-22 18:02:00 +0100

    update readme

--- TypeScript/app/items/backstage.update.ts
+++ TypeScript/app/items/backstage.update.ts
@@ -2,21 +2,20 @@ import { IItem } from "@/interface/item";
 import { IItemUpdate } from "@/interface/item.update";
 
 export class BackStageQualityUpdate implements IItemUpdate {
-    updateQuality(item: IItem): void {
-      if (item.quality < 50) {
+  updateQuality(item: IItem): void {
+    if (item.quality < 50) {
+      item.quality++;
+
+      if (item.sellIn < 11) {
         item.quality++;
-  
-        if (item.sellIn < 11) {
-          item.quality++;
-        }
-  
-      }
-  
-      item.sellIn--;
-  
-      if (item.sellIn < 0) {
-        item.quality = item.quality - item.quality;
       }
     }
-  
+
+    item.sellIn--;
+
+    if (item.sellIn < 0) {
+      item.quality -= item.quality;
+    }
   }
+
+}
